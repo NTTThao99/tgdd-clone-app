9851ecc5361342260a8c16a1df91e04d02d8b60d
NAM TRUONG DUONG <namtruong@namtruong-pc>
2020-11-08 14:03:05 +0700

    sua lai hieu ung hover, tham the main, can giua header

---
+++ main.js
@@ -0,0 +1,20 @@
+// const productHover = document.querySelectorAll('.sale__hotest__item a > img');
+
+// const imageEnter = (item) => {
+//   item.style.transition = 'all .3s ease';
+//   item.style.transform = 'translateY(-10px)';
+// };
+
+// const imageLeave = (item) => {
+//   item.style.transition = 'all .3s ease';
+//   item.style.transform = 'translateY(0)';
+// };
+
+// for (const i of productHover) {
+//   i.addEventListener('mouseenter', () => {
+//     imageEnter(i);
+//   });
+//   i.addEventListener('mouseleave', () => {
+//     imageLeave(i);
+//   });
+// }
